6bc7611353dd9623e18ee1d5d8294b9cf1a1a4b6
danielkhan-v1 <grigorovich314@gmail.com>
2022-02-08 23:06:11 +0100

    "fix start.sh"

--- start.sh
+++ start.sh
@@ -3,3 +3,9 @@
 sudo sudo apt update
 sudo apt install ansible
 sudo apt upgrade -y
+
+cd ~/lab-k8s/ansible/
+ansible-playbook ansible-book.yml
+sudo usermod -a -G microk8s ubuntu
+sudo chown -f -R ubuntu ~/.kube
+newgrp microk8s
