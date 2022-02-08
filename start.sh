05c5ce31d954b1eda41b966da3620cb1b288e39a
danielkhan-v1 <grigorovich314@gmail.com>
2022-02-08 23:59:24 +0100

    "fix ansible-book.yml"

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ sudo apt install ansible
 sudo apt upgrade -y
 
 cd ~/lab-k8s/ansible/
+ansible-galaxy collection install kubernetes.core
 ansible-playbook ansible-book.yml
 sudo usermod -a -G microk8s ubuntu
 sudo chown -f -R ubuntu ~/.kube
