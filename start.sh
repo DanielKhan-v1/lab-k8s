66ca1382713f0ec4eef6d387a9f0bfdd0ffaf239
danielkhan-v1 <grigorovich314@gmail.com>
2022-02-09 01:58:46 +0100

    "fix start.sh"

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ sudo apt install ansible
 sudo apt upgrade -y
 
 ansible-galaxy collection install kubernetes.core
+mkdir ~/.kube
+touch ~/.kube/config
 #microk8s kubectl config view --raw >~/.kube/config
 ansible-playbook ~/lab-k8s/ansible/ansible-book.yml
 sudo usermod -a -G microk8s ubuntu
