683e0b6095b5c95735f49624ac682e4593547d93
danielkhan-v1 <grigorovich314@gmail.com>
2022-02-13 18:46:31 +0100

    fix: terraform files

--- ec2-infrastructure/main.tf
+++ ec2-infrastructure/main.tf
@@ -102,21 +102,6 @@ resource "aws_key_pair" "deployer" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQClguu9v4rFaVaoc1mPn++1KwvaoQOWyZvgH7NF0v2O5lAAjlhYpVmi4QeJS5gsy4xzbVWJO8iNPFOYlP38dIhlCC9iNaOn8BH9mtPv7ZAg1raGI9QySoHiaTPa0DkBqjoXoiiXBULlrQ4ccf5uMT0BLRbzSGj2cPYshUDnomx9Yr51Zzrm3QyKxFnl889CugLhr+LWYeEN9GM3qEpQKLcFikCrxMr8eKQMzURt9Cg1TEjskHRZJK4ITYoxXCARXiBsX1feiPbFBOQ0fPM4Xa9tyD0Vi5NstM90WYcoYh+32UElqcTlcEv5I1DBQ1uTwXfs30TSvliJRKa+5im+gWVTj9bP21eRM+ul+4I5uUDD46NyNx1V6d2KW9rdRMPyWLjOUvTzkIPgvqWbj2/y0Md1bol9UzNLf1tIf6COQVj2WPtFUWJI/wj9cGcwgn+4j7EKTMxwTFveDAEvaFlPxKF+ai6u4gzgHDV1VNRPfvxErSqcI/MwvZwitLalUymQ17c= grigorovich314@gmail.com"
 }
 
-resource "aws_ebs_volume" "ebs_volume" {
-  availability_zone = "${var.region}a"
-  size = 30
-
-  tags = {
-    Name = "lab-ebs-daniil"
-  }
-}
-
-resource "aws_volume_attachment" "valume-attach" {
-  device_name = "/dev/sdh"
-  instance_id = aws_instance.app_server.id
-  volume_id   = aws_ebs_volume.ebs_volume.id
-}
-
 resource "aws_instance" "app_server" {
   ami                  = var.ami
   instance_type        = var.ami_instance_type
@@ -125,7 +110,7 @@ resource "aws_instance" "app_server" {
   iam_instance_profile = "jenkins"
   depends_on           = [aws_internet_gateway.gateway]
   root_block_device {
-    volume_size = "20"
+    volume_size = "40"
   }
   tags = {
     Name = "lab-EC2_instance-daniil"
